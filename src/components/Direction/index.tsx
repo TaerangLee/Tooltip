979915a3b705069f5bbd50ca4218261cb2183970
TaerangLee <s22051@gsm.hs.kr>
2024-05-25 01:19:12 +0900

    Add style Direction component

---
+++ src/components/Direction/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as S from "./style";
+
+const Direction = () => {
+  return <S.Direction />;
+};
+
+export default Direction;
---
+++ src/components/Direction/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Direction = styled.div`
+  width: 0;
+  height: 0;
+  border-bottom: 4px solid transparent;
+  border-top: 4px solid transparent;
+  border-left: 4px solid #1a2735;
+  border-right: 4px solid transparent;
+`;
