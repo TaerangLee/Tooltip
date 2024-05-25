8445d51752700547871387c5073cd8e7a512cd72
TaerangLee <s22051@gsm.hs.kr>
2024-05-26 03:06:01 +0900

    Update Section2

---
+++ src/components/Section2/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import * as S from "./style";
+import Section1 from "../Section1";
+
+const Section2 = () => {
+  return (
+    <S.Section2Wrapper>
+      <Section1 />
+    </S.Section2Wrapper>
+  );
+};
+
+export default Section2;
---
+++ src/components/Section2/style.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Section2Wrapper = styled.div`
+  margin-left: 40px;
+  height: 100px;
+  overflow: scroll;
+`;
