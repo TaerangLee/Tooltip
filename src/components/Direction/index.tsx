9a231bc36105de3f31c9e6931144d122a6fe4b29
TaerangLee <s22051@gsm.hs.kr>
2024-05-25 17:02:33 +0900

    Add style direction style conpoment

--- src/components/Direction/index.tsx
+++ src/components/Direction/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 import * as S from "./style";
+import { TooltipPosition } from "../../types/Direction";
 
-const Direction = () => {
-  return <S.Direction />;
+const Direction = ({
+  direction,
+  directionSlice,
+}: {
+  direction: string | undefined;
+  directionSlice: string | undefined;
+}) => {
+  const x = direction && TooltipPosition[direction]?.x;
+  const y = direction && TooltipPosition[direction]?.y;
+
+  console.log(x);
+  console.log(y);
+
+  return <S.Direction x={x} y={y} directionSlice={directionSlice} />;
 };
 
 export default Direction;
--- src/components/Direction/style.ts
+++ src/components/Direction/style.ts
@@ -1,10 +1,25 @@
 import styled from "@emotion/styled";
 
-export const Direction = styled.div`
+export const Direction = styled.div<{
+  directionSlice: string | undefined;
+  x: string | undefined;
+  y: string | undefined;
+}>`
+  position: absolute;
   width: 0;
   height: 0;
-  border-bottom: 4px solid transparent;
-  border-top: 4px solid transparent;
-  border-left: 4px solid #1a2735;
-  border-right: 4px solid transparent;
+  border-bottom: 5px solid transparent;
+  border-top: 5px solid transparent;
+  border-left: 5px solid #1a2735;
+  border-right: 5px solid transparent;
+
+  transform: ${({ x, y, directionSlice }) =>
+    `translateX(${x}px) translateY(${y}px)` +
+    (directionSlice === "R"
+      ? " rotate(180deg)"
+      : directionSlice === "B"
+      ? " rotate(270deg)"
+      : directionSlice === "L"
+      ? " rotate(0deg)"
+      : " rotate(90deg)")};
 `;
