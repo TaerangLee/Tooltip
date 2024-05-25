add206610afeecf4fdd4eb670e565327fa33f6ad
TaerangLee <s22051@gsm.hs.kr>
2024-05-26 03:07:52 +0900

    Add CheckContent Types 4 directions

--- src/types/Direction.ts
+++ src/types/Direction.ts
@@ -14,16 +14,23 @@ export enum TooltipDirection {
 }
 
 export const TooltipPosition: { [key: string]: { x: string; y: string } } = {
-  TL: { x: "0", y: "32" },
-  Top: { x: "-25", y: "32" },
-  TR: { x: "25", y: "32" },
-  RT: { x: "-40", y: "-17" },
-  Right: { x: "-40", y: "0" },
-  RB: { x: "-40", y: "17" },
-  BR: { x: "25", y: "-32" },
-  Bottom: { x: "0", y: "-32" },
-  BL: { x: "-25", y: "-32" },
-  LB: { x: "40.1", y: "17" },
-  Left: { x: "40.1", y: "0" },
-  LT: { x: "40.1", y: "-17" },
+  TL: { x: "-25", y: "31" },
+  Top: { x: "0", y: "31" },
+  TR: { x: "25", y: "31" },
+  RT: { x: "-39", y: "-17" },
+  Right: { x: "-39", y: "0" },
+  RB: { x: "-39", y: "17" },
+  BR: { x: "25", y: "-31" },
+  Bottom: { x: "0", y: "-31" },
+  BL: { x: "-25", y: "-31" },
+  LB: { x: "40", y: "17" },
+  Left: { x: "40", y: "0" },
+  LT: { x: "40", y: "-17" },
+};
+
+export const CheckContent: { [key: string]: { x: string; y: string } } = {
+  Top: { x: "0", y: "44" },
+  Right: { x: "-142", y: "0" },
+  Bottom: { x: "0", y: "-44" },
+  Left: { x: "142", y: "0" },
 };
