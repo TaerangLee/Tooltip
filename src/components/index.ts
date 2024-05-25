d7f48d64ff26bbfb804b324627c37d17f0ef53e1
TaerangLee <s22051@gsm.hs.kr>
2024-05-25 17:03:34 +0900

    Add export compoents, pages

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as TooltipButton } from "./TooltipButton";
 export { default as Content } from "./Content";
+export { default as Direction } from "./Direction";
+export { default as Section1 } from "./Section1";
