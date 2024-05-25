512fe96c1899e971ba3fe5d7cf5da8b0675f4d05
TaerangLee <s22051@gsm.hs.kr>
2024-05-25 17:02:01 +0900

    Add Section part 1

---
+++ src/components/Section1/style.ts
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+
+export const Section1Wrapper = styled.div`
+  width: 360px;
+  height: 125px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TopBottomContainer = styled.div`
+  display: flex;
+  gap: 3px;
+`;
+
+export const SideContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const LeftRightSide = styled.div`
+  display: flex;
+  gap: 3px;
+  flex-direction: column;
+
+  .LeftOnly {
+    width: 100%;
+  }
+`;
