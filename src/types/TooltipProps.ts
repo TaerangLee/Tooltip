d61ec0f233a38c1116f2b160c636ebb38ce855e7
TaerangLee <s22051@gsm.hs.kr>
2024-05-25 17:01:43 +0900

    Update setting types

--- src/types/TooltipProps.ts
+++ src/types/TooltipProps.ts
@@ -1,5 +1,6 @@
 export interface TooltipProps {
-  children: string; // 내부 적인 button 내용
-  width: "short" | "long"; // 버튼을 한 개로 관리하기 위한 props types
+  children: React.ReactNode;
+  width?: "long";
   color?: string;
+  direction?: string;
 }
