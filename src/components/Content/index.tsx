2dbc67fa1a6bdab08ead888efd080c74007f3584
TaerangLee <s22051@gsm.hs.kr>
2024-05-27 23:13:44 +0900

    Update style content component

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -3,17 +3,32 @@ import React from "react";
 import * as S from "./style";
 import Direction from "../Direction";
 
-const Content = ({ direction }: { direction: string | undefined }) => {
+const Content = ({
+  direction,
+  type,
+}: {
+  direction?: string;
+  type?: string;
+}) => {
   const directionSlice = direction?.slice(0, 1);
-  console.log(directionSlice);
 
   return (
     <>
       <S.ContentWrapper direction={directionSlice} sideDirection={direction}>
-        <S.ContentContainer>
-          <span>prompt text</span>
-          <span>prompt text</span>
-          <span>prompt text</span>
+        <S.ContentContainer type={type}>
+          {type ? (
+            <S.FontContainer>
+              <span> Lorem ipsum dolor sit amet, </span>
+              <span> consectetur adipisicing elit. Adipisci</span>
+              <span>asperiores atque</span>
+            </S.FontContainer>
+          ) : (
+            <S.FontContainer>
+              <span>prompt tasd</span>
+              <span>prompt tasd</span>
+              <span>prompt tasd</span>
+            </S.FontContainer>
+          )}
         </S.ContentContainer>
         <Direction direction={direction} directionSlice={directionSlice} />
       </S.ContentWrapper>
--- src/components/Content/style.ts
+++ src/components/Content/style.ts
@@ -1,11 +1,34 @@
 import styled from "@emotion/styled";
 
+export const FontContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 3px;
+`;
+
+export const ContentContainer = styled.div<{ type?: string }>`
+  width: auto;
+  min-width: ${(props) => (props.type ? "200px" : "none")};
+  min-height: 40px;
+  padding: 5px 4px 7px 5px;
+  display: flex;
+
+  span {
+    color: white;
+    font-size: 10px;
+    display: flex;
+    justify-content: center;
+  }
+`;
 export const ContentWrapper = styled.div<{
   direction: string | undefined;
   sideDirection: string | undefined;
 }>`
-  width: 70px;
-  z-index: 2;
+  min-width: 70px;
+  width: auto;
+  z-index: 1;
+
   background-color: #1a2735;
   border-radius: 5px;
   display: flex;
@@ -20,19 +43,4 @@ export const ContentWrapper = styled.div<{
       : props.direction === "R"
       ? "translateX(80px) translateY(-20px)"
       : "translateY(-80px)"};
-`;
-
-export const ContentContainer = styled.div`
-  width: 62px;
-  padding: 5px 4px 7px 4px;
-  display: flex;
-  flex-direction: column;
-  gap: 3px;
-
-  span {
-    color: white;
-    font-size: 10px;
-    display: flex;
-    justify-content: center;
-  }
 `;
