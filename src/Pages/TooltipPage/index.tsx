314b54b27f30476cd3d5e46dbca17cb1bc1cb430
TaerangLee <s22051@gsm.hs.kr>
2024-05-27 23:31:06 +0900

    Update app, pages base settings

--- src/Pages/TooltipPage/index.tsx
+++ src/Pages/TooltipPage/index.tsx
@@ -1,17 +1,28 @@
 import * as S from "./style";
 
-import { Section1 } from "../../components";
+import {
+  Section1,
+  Section2,
+  Section3,
+  Section4,
+  Section5,
+  Section6,
+} from "../../components";
 
 // import { TooltipDirection } from "../../types/Direction";
 
 const TooltipPage = () => {
   return (
     <S.TooltipWrapper>
-      <div>
-        {/* <TooltipButton width="long">enter-delay 1s</TooltipButton> */}
-        <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", flexDirection: "column", gap: "100px" }}>
+        <div style={{ display: "flex", gap: "70px" }}>
           <Section1 />
+          <Section2 />
         </div>
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Section6 />
       </div>
     </S.TooltipWrapper>
   );
