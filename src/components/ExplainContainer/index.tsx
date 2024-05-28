ba0c9fa6f2f9f3a03903c639520dd1b130bad36b
TaerangLee <s22051@gsm.hs.kr>
2024-05-28 12:04:15 +0900

    Update explainContainer appoint style, props

--- src/components/ExplainContainer/index.tsx
+++ src/components/ExplainContainer/index.tsx
@@ -4,13 +4,15 @@ import * as S from "./style";
 import Content from "../Content";
 import { ExplainProps } from "../../types/ExplainProps";
 
-const ExplainContainer = ({ onMouse, isHover }: ExplainProps) => {
+const ExplainContainer = ({ onMouse, isHover, albeWord }: ExplainProps) => {
   return (
     <S.ExplainContainer onMouseEnter={onMouse} onMouseLeave={onMouse}>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
       asperiores atque
       <S.ExplainContent>
-        {isHover && <Content direction="Top" type="explain" />}
+        {isHover && albeWord === "Disable" && (
+          <Content direction="Top" type="explain" />
+        )}
       </S.ExplainContent>
     </S.ExplainContainer>
   );
