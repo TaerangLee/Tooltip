aaa542763e38e8d27c3ee89fd35dab3f1656bbed
TaerangLee <s22051@gsm.hs.kr>
2024-05-28 09:09:19 +0900

    Add export types

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./CheckoutContentProps";
+export * from "./ColorContentProps";
+export * from "./ContentProps";
+export * from "./Direction";
+export * from "./DirectionProps";
+export * from "./ExplainProps";
+export * from "./TooltipProps";
